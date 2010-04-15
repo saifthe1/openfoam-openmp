fe867766d93a680223e755811fbb824dfb7174e3
Per.Zut <Per.Zut@95e6e318-8b5d-c1e6-a0b8-c47b7b38f910>
2010-04-15 06:28:48 +0000

    Update, nu är det hela filen.

--- dieselFoam_preCompiled.C
+++ dieselFoam_preCompiled.C
@@ -1,5 +1,248 @@
-    // .
-	while (runTime.run())
+int main(int argc, char *argv[])
+{
+
+
+
+# 1 "../dieselEngineFoam/createFields.H" 1
+Info<< nl << "Reading thermophysicalProperties" << endl;
+
+autoPtr<psiChemistryModel> pChemistry
+(
+    psiChemistryModel::New(mesh)
+);
+psiChemistryModel& chemistry = pChemistry();
+
+hCombustionThermo& thermo = chemistry.thermo();
+
+basicMultiComponentMixture& composition = thermo.composition();
+PtrList<volScalarField>& Y = composition.Y();
+
+word inertSpecie(thermo.lookup("inertSpecie"));
+
+volScalarField rho
+(
+    IOobject
+    (
+        "rho",
+        runTime.timeName(),
+        mesh
+    ),
+    thermo.rho()
+);
+
+Info<< "Reading field U\n" << endl;
+volVectorField U
+(
+    IOobject
+    (
+        "U",
+        runTime.timeName(),
+        mesh,
+        IOobject::MUST_READ,
+        IOobject::AUTO_WRITE
+    ),
+    mesh
+);
+
+
+volScalarField& p = thermo.p();
+const volScalarField& psi = thermo.psi();
+const volScalarField& T = thermo.T();
+volScalarField& h = thermo.h();
+
+
+# 1 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/compressibleCreatePhi.H" 1
+# 38 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/compressibleCreatePhi.H"
+Info<< "Reading/calculating face flux field phi\n" << endl;
+
+surfaceScalarField phi
+(
+    IOobject
+    (
+        "phi",
+        runTime.timeName(),
+        mesh,
+        IOobject::READ_IF_PRESENT,
+        IOobject::AUTO_WRITE
+    ),
+    linearInterpolate(rho*U) & mesh.Sf()
+);
+# 49 "../dieselEngineFoam/createFields.H" 2
+
+volScalarField kappa
+(
+    IOobject
+    (
+        "kappa",
+        runTime.timeName(),
+        mesh,
+        IOobject::NO_READ,
+        IOobject::AUTO_WRITE
+    ),
+    mesh,
+    dimensionedScalar("zero", dimless, 0.0)
+);
+
+Info << "Creating turbulence model.\n" << nl;
+autoPtr<compressible::turbulenceModel> turbulence
+(
+    compressible::turbulenceModel::New
+    (
+        rho,
+        U,
+        phi,
+        thermo
+    )
+);
+
+Info<< "Creating field DpDt\n" << endl;
+volScalarField DpDt =
+    fvc::DDt(surfaceScalarField("phiU", phi/fvc::interpolate(rho)), p);
+
+
+multivariateSurfaceInterpolationScheme<scalar>::fieldTable fields;
+
+forAll (Y, i)
+{
+    fields.add(Y[i]);
+}
+fields.add(h);
+# 53 "dieselFoam.C" 2
+# 1 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/readGravitationalAcceleration.H" 1
+    Info << "\nReading g" << endl;
+    uniformDimensionedVectorField g
+    (
+        IOobject
+        (
+            "g",
+            runTime.constant(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
+# 54 "dieselFoam.C" 2
+# 1 "../dieselEngineFoam/readCombustionProperties.H" 1
+Info<< "Reading combustion properties\n" << endl;
+
+IOdictionary combustionProperties
+(
+    IOobject
+    (
+        "combustionProperties",
+        runTime.constant(),
+        mesh,
+        IOobject::MUST_READ,
+        IOobject::NO_WRITE
+    )
+);
+
+dimensionedScalar Cmix
+(
+    combustionProperties.lookup("Cmix")
+);
+# 55 "dieselFoam.C" 2
+# 1 "../dieselEngineFoam/createSpray.H" 1
+Info << "Constructing Spray" << endl;
+
+PtrList<gasThermoPhysics> gasProperties(Y.size());
+forAll(gasProperties, i)
+{
+    gasProperties.set
+    (
+        i,
+        new gasThermoPhysics
+        (
+            dynamic_cast<const reactingMixture<gasThermoPhysics>&>
+                (thermo).speciesData()[i]
+        )
+    );
+}
+
+spray dieselSpray
+(
+    U,
+    rho,
+    p,
+    T,
+    composition,
+    gasProperties,
+    thermo,
+    g
+);
+
+scalar gasMass0 = fvc::domainIntegrate(rho).value();
+
+if (dieselSpray.twoD())
+{
+    gasMass0 *= 2.0*mathematicalConstant::pi/dieselSpray.angleOfWedge();
+}
+
+gasMass0 -=
+    dieselSpray.injectedMass(runTime.value()) - dieselSpray.liquidMass();
+# 56 "dieselFoam.C" 2
+# 1 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/initContinuityErrs.H" 1
+# 38 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/initContinuityErrs.H"
+scalar cumulativeContErr = 0;
+# 57 "dieselFoam.C" 2
+# 1 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/readTimeControls.H" 1
+# 33 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/readTimeControls.H"
+Switch adjustTimeStep
+(
+    runTime.controlDict().lookup("adjustTimeStep")
+);
+
+scalar maxCo
+(
+    readScalar(runTime.controlDict().lookup("maxCo"))
+);
+
+scalar maxDeltaT =
+    runTime.controlDict().lookupOrDefault<scalar>("maxDeltaT", GREAT);
+# 58 "dieselFoam.C" 2
+# 1 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/compressibleCourantNo.H" 1
+# 33 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/compressibleCourantNo.H"
+scalar CoNum = 0.0;
+scalar meanCoNum = 0.0;
+
+if (mesh.nInternalFaces())
+{
+    surfaceScalarField SfUfbyDelta =
+        mesh.surfaceInterpolation::deltaCoeffs()*mag(phi)/fvc::interpolate(rho);
+
+    CoNum = max(SfUfbyDelta/mesh.magSf())
+        .value()*runTime.deltaT().value();
+
+    meanCoNum = (sum(SfUfbyDelta)/sum(mesh.magSf()))
+        .value()*runTime.deltaT().value();
+}
+
+Info<< "Courant Number mean: " << meanCoNum
+    << " max: " << CoNum << endl;
+# 59 "dieselFoam.C" 2
+# 1 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/setInitialDeltaT.H" 1
+# 34 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/setInitialDeltaT.H"
+if (adjustTimeStep)
+{
+    if ((runTime.timeIndex() == 0) && (CoNum > SMALL))
+    {
+        runTime.setDeltaT
+        (
+            min
+            (
+                maxCo*runTime.deltaT().value()/CoNum,
+                maxDeltaT
+            )
+        );
+    }
+}
+# 60 "dieselFoam.C" 2
+
+
+
+    Info << "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
     {
 # 1 "/home/openfoam/OpenFOAM/OpenFOAM-1.6/src/finiteVolume/lnInclude/readPISOControls.H" 1
     dictionary piso = mesh.solutionDict().subDict("PISO");
