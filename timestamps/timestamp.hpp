21f11f5a7147f5c08cb28e3563538667e4ba8eda
Per.Zut <Per.Zut@95e6e318-8b5d-c1e6-a0b8-c47b7b38f910>
2010-04-22 12:28:36 +0000

    Laggt till timestamp-biblioteket. Det är najs!

---
+++ timestamps/timestamp.hpp
@@ -0,0 +1,48 @@
+
+// Includes
+#include <omp.h>
+#include <iostream>
+#include <iomanip>
+#include <map>
+
+#define TS_START ts::get_instance()->start
+#define TS_END ts::get_instance()->end
+#define TS_TOGGLE ts::get_instance()->toggle
+#define TS_PRINT ts::get_instance()->printStamps
+
+class ts_obj
+{
+	public:
+		unsigned int stamps;
+		double time, lastStamp;
+		ts_obj() : stamps(0), time(0), lastStamp(0) {}
+		ts_obj(const double curTime) : stamps(0), time(0), lastStamp(curTime) {}
+};
+
+class ts
+{
+	private:
+		
+		std::map<std::string, ts_obj> stamps;
+		
+		// Singelton
+		ts() : ok(true) {};
+		ts(const ts&) : ok(true) {};
+		static ts* instance;
+
+		bool ok;
+		
+	public:
+		void start(const std::string& stamp_name);
+		void end(const std::string& stamp_name);
+
+		void toggle();
+		void toggle(bool);
+
+		const std::map<std::string, ts_obj> getStamps();
+		void printStamps();
+
+		static ts* get_instance();
+};
+
+
